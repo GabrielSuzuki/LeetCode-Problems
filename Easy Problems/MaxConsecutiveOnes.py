2d548d8370ca5f2a4ca2b0539f3b8a0e8184c921
GabrielSuzuki <suzukig@csu.fullerton.edu>
2023-03-09 09:31:31 -0800

    Create MaxConsecutiveOnes.py

---
+++ Easy Problems/MaxConsecutiveOnes.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maximum = 0
+        count = 0
+        for i in nums:
+            if i == 1:
+                count += 1
+            else:
+                maximum = max(maximum,count)
+                count = 0
+        maximum = max(maximum,count)
+        return maximum
